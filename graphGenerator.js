5be8ad456fb6d80a68ce93e7dbf60893ca05d959
panicboy <asasaki@gmail.com>
2016-08-08 15:04:44 -1000

    first commit; updated README

--- graphGenerator.js
+++ graphGenerator.js
@@ -1,1 +1,20 @@
 // Implement a graph generator that represents a set of objects where some pairs of objects are connected by links.
+
+// tests whether there is an edge from the vertices x to y;
+function adjacent(G, x, y) {
+
+}
+
+// lists all vertices such that there is an edge from the vertices x to y
+function neighbors(G, x) {
+
+}
+
+
+// adds the vertex x, if it is not there;
+function add_vertex(G, x) {
+
+}
+
+// removes the vertex x, if it is there;
+function remove_vertex(G, x){}
